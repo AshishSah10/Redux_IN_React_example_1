69792161f3dd2f4022ae0a4d8375b45d0fee2f0d
AshishSah10 <41590024+AshishSah10@users.noreply.github.com>
2021-05-14 14:53:41 +0530

    initial commit -simple counter using redux

--- src/App.js
+++ src/App.js
@@ -5,26 +5,28 @@ export default function App() {
   const [count, setCount] = useState(0);
 
   const reducer = (state, action) => {
-    switch(action.type){
+    switch (action.type) {
       case "INCREASE":
         state = action.payload + 1;
         return state;
       default:
         return state;
     }
-  }
+  };
   const dispatch = (reducerObject) => {
-    const {type, payload} = reducerObject;
+    const { type, payload } = reducerObject;
     const updatedState = reducer(count, reducerObject);
-    console.log(updatedState)
-    setCount(updatedState)
-  }
+    console.log(updatedState);
+    setCount(updatedState);
+  };
   return (
     <div className="App">
-      <button onClick={() => dispatch({type:"INCREASE",payload: count})}>Up</button>
-      <br/>
+      <button onClick={() => dispatch({ type: "INCREASE", payload: count })}>
+        Up
+      </button>
+      <br />
       {count}
-      <br/>
+      <br />
       <button onClick={() => setCount(count - 1)}>Down</button>
     </div>
   );
